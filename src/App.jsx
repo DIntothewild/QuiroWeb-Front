7940755f8bc9a7ee177355b1d40da7cf480868e4
David Rabaneda Carrillo <davidrabanedacarrillo@Davids-MacBook-Air.local>
2025-05-27 20:27:50 +0200

    Style: improve desktop header layout with left-aligned logo and larger subtitle text 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Home from './Views/Home.jsx';
 import './App.css';
 import BookingComponent from './Components/BookingComponent.jsx';
 
 function App() {
+  console.log("✅ App.js está renderizando"); // Verifica si esto aparece en la consola
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src="/images/logo-transp.svg" alt="Wellness Flow logo" className="logo" />
-          <div className="header-text">
-            <h1>WELLNESS FLOW</h1>
-            <h2 className="subtitle">En construcción (coming soon)</h2>
-            <h3 className="tagline">Un enfoque en el cuidado natural y el equilibrio personal.</h3>
-            <h2 className="booking">Booking site</h2>
-          </div>
-        </header>
+  <img src="/images/logo-transp.svg" alt="Wellness Flow logo" className="logo" />
+  <div className="header-text">
+    <h1>WELLNESS FLOW</h1>
+    <h2 className="subtitle">En construcción (coming soon)</h2>
+    <h3 className="tagline">Un enfoque en el cuidado natural y el equilibrio personal.</h3>
+    <h2>Booking site</h2>
+  </div>
+</header>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
